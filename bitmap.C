42e23c0c71508e797bf02dd450ef1db79ca26154
Nipin L <nipinl@gmail.com>
2021-05-21 02:04:39 +0530

    tut 76

--- bitmap.C
+++ bitmap.C
@@ -1,7 +1,7 @@
 #include"bitmap.H"
 namespace advCppCourse{
     
-    bitmap::bitmap(int width, int height):m_width(width), m_height(height)
+    bitmap::bitmap(int width, int height):m_width(width), m_height(height), m_pPixels(new uint8_t[width*height*3])
     {
     }
     void setPixel(int x, int y, uint8_t red, uint8_t green, uint8_t blue){
