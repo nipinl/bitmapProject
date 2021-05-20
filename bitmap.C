857a4e1b58c07044fd4d9148c29c06832d1d94db
Nipin L <nipinl@gmail.com>
2021-05-21 01:46:58 +0530

    tut 75

---
+++ bitmap.C
@@ -0,0 +1,17 @@
+#include"bitmap.H"
+namespace advCppCourse{
+    
+    bitmap::bitmap(int width, int height):m_width(width), m_height(height)
+    {
+    }
+    void setPixel(int x, int y, uint8_t red, uint8_t green, uint8_t blue){
+
+    }
+    bool bitmap::write(string fileName){
+        return false;
+    }
+    bitmap::~bitmap()
+    {
+    }
+    
+}
