9397f63e269119c74e9b5c9f020a9917e2de88bc
Nipin L <nipinl@gmail.com>
2021-05-21 15:59:54 +0530

    started implementing Mandelbrot class

---
+++ Mandelbrot.C
@@ -0,0 +1,10 @@
+#include"Mandelbrot.H"
+namespace advCppCourse{
+    
+    Mandelbrot::Mandelbrot(){}
+    Mandelbrot::~Mandelbrot(){}
+    int getIterations(double x, double y){
+        
+    }
+    
+}
