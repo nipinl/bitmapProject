41444932757f7e421dcef98655b0b83a72c68482
Nipin L <nipinl@gmail.com>
2021-05-21 13:05:15 +0530

    written a blanjk bitmap file

--- fractal.C
+++ fractal.C
@@ -1,9 +1,12 @@
 //name : fractal.C
 #include<iostream>
-#include"bitmapFileHeader.H"
-#include"bitmapInfoHeader.H"
+#include"bitmap.H"
+
 
 using namespace std;
+using namespace advCppCourse;
 int main(){
-    cout<<"hello"<<endl;
+    bitmap bitmap1(800,600);
+    bitmap1.write("test.bmp");
+    cout<<"finished"<<endl;
 }
