db10da2d9fec0feefbb8365c75084fc5ac28974f
Nipin L <nipinl@gmail.com>
2021-05-21 15:47:16 +0530

    scaling

--- fractal.C
+++ fractal.C
@@ -11,11 +11,20 @@ int main(){
     const int H = 600;
     bitmap bitmap1(W,H);
     bitmap1.setPixel(W/2, H/2, 255,255,255);
-    for(int i=0;i<W;i++){
-        for(int j=0;j<H;j++){
-            bitmap1.setPixel(i,j,120,200,100);
+
+    double min{1e5}, max{-1e5};
+    for(int x=0;x<W;x++){
+        for(int y=0;y<H;y++){
+            //bitmap1.setPixel(i,j,120,200,100);//fav green color
+            double xFractal = (x - W/2)*(2.0/W);
+            double yFractal = (y - H/2)*(2.0/H);
+            if (xFractal<min) min=xFractal;
+            if (xFractal>max) max=xFractal;
+            if (yFractal<min) min=yFractal;
+            if (yFractal>max) max=yFractal;
         }
     }
+    cout<<min<<","<<max<<endl;
     bitmap1.write("test.bmp");
     cout<<"finished"<<endl;
 }
