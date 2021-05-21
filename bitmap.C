a054f7a323c1878a1d241281d4c41169c85c599c
Nipin L <nipinl@gmail.com>
2021-05-21 14:28:04 +0530

    another

--- bitmap.C
+++ bitmap.C
@@ -8,9 +8,16 @@ namespace advCppCourse{
     bitmap::bitmap(int width, int height):m_width(width), m_height(height), m_pPixels(new uint8_t[width*height*3]{})
     {
     }
-    void setPixel(int x, int y, uint8_t red, uint8_t green, uint8_t blue){
+
+    void bitmap::setPixel(int x, int y, uint8_t red, uint8_t green, uint8_t blue){
+        uint8_t *pPixel =  m_pPixels.get();
+        pPixel += (y*3)*m_width + (x*3);
+        pPixel[0] = blue;
+        pPixel[1] = green;
+        pPixel[2] = red;
 
     }
+
     bool bitmap::write(string fileName){
         bitmapFileHeader fileHeader;
         bitmapInfoHeader infoHeader;
--- fractal.C
+++ fractal.C
@@ -5,8 +5,17 @@
 
 using namespace std;
 using namespace advCppCourse;
+
 int main(){
-    bitmap bitmap1(800,600);
+    const int W = 800;
+    const int H = 600;
+    bitmap bitmap1(W,H);
+    bitmap1.setPixel(W/2, H/2, 255,255,255);
+    for(int i=0;i<W;i++){
+        for(int j=0;j<H;j++){
+            bitmap1.setPixel(i,j,122,200,100);
+        }
+    }
     bitmap1.write("test.bmp");
     cout<<"finished"<<endl;
 }
