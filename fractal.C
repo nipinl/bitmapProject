ab0e48cbf482f82225973ad42199a6ec6515d7e3
Nipin L <nipinl@gmail.com>
2021-05-21 17:35:23 +0530

    stored iterPerPixel

--- fractal.C
+++ fractal.C
@@ -17,14 +17,18 @@ int main(){
 
     double min{1e5}, max{-1e5};
     unique_ptr<int[]>histogram(new int[Mandelbrot::MAX_ITER+1]{0});//with initialisation to 0
+    unique_ptr<int[]>iterPerPixel(new int[W*H]{0});//with initialisation to 0
     for(int x=0;x<W;x++){
         for(int y=0;y<H;y++){
             //bitmap1.setPixel(i,j,120,200,100);//fav green color
             double xFractal = (x - W/2-W/4)*(2.0/H);
             double yFractal = (y - H/2)*(2.0/H);
 
+
             int iter = Mandelbrot::getIterations(xFractal,yFractal);
-            histogram[iter]++;
+            iterPerPixel[y*W+x] = iter;//storing the iterations
+            if(iter!=Mandelbrot::MAX_ITER) histogram[iter]++;//to avoid the last entry showing no. of pixels going to inf
+
             
             uint8_t color = (uint8_t)(256*(double) iter/Mandelbrot::MAX_ITER);
             color = color*color*color;//max will be 255 since uint_8 has a max 255
@@ -36,15 +40,7 @@ int main(){
             if (yFractal>max) max=yFractal; */
         }
     }
-    cout<<endl;
-    int count =0;
-    for (int i=0; i<=Mandelbrot::MAX_ITER;i++){
-        count += histogram[i];
-        cout<<histogram[i]<<" ;      "<<flush;
-    }
-    cout<<" sum "<<endl;
-    cout<<count<<"  "<<W*H<<endl;
-    cout<<min<<","<<max<<endl;
+    
     bitmap1.write("test.bmp");
     cout<<"finished"<<endl; 
 }
