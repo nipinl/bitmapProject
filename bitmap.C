c1f826c79c5440999f110b696f33eb4f82d57483
Nipin L <nipinl@gmail.com>
2021-05-21 11:12:47 +0530

    tut77

--- bitmap.C
+++ bitmap.C
@@ -1,17 +1,26 @@
 #include"bitmap.H"
+#include"bitmapFileHeader.H"
+#include"bitmapInfoHeader.H"
+using namespace advCppCourse;
 namespace advCppCourse{
     
-    bitmap::bitmap(int width, int height):m_width(width), m_height(height), m_pPixels(new uint8_t[width*height*3])
+    bitmap::bitmap(int width, int height):m_width(width), m_height(height), m_pPixels(new uint8_t[width*height*3]{})
     {
     }
     void setPixel(int x, int y, uint8_t red, uint8_t green, uint8_t blue){
 
     }
     bool bitmap::write(string fileName){
+        bitmapFileHeader fileHeader;
+        bitmapInfoHeader infoHeader;
+        fileHeader.fileSize = sizeof(bitmapInfoHeader)+sizeof(bitmapFileHeader)+m_width*m_height*3;
+        fileHeader.dataOffset = sizeof(bitmapInfoHeader)+sizeof(bitmapFileHeader);
+        
+        infoHeader.width = m_width;
+        infoHeader.height = m_height;
+
         return false;
-    }
-    bitmap::~bitmap()
-    {
-    }
+        }
+    bitmap::~bitmap(){}
     
 }
