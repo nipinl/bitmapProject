7b214233bbda81c26aaf2915d2a36807bc9a3330
Nipin L <nipinl@gmail.com>
2021-05-21 16:23:03 +0530

    implementing iterations

--- Mandelbrot.C
+++ Mandelbrot.C
@@ -1,10 +1,26 @@
+#include<complex>
 #include"Mandelbrot.H"
+using namespace std;
 namespace advCppCourse{
     
     Mandelbrot::Mandelbrot(){}
     Mandelbrot::~Mandelbrot(){}
     int getIterations(double x, double y){
-        
+
+        complex<double> z =0;
+        complex<double> c(x,y);
+        int iter = 0;
+        while(iter<MAX_ITER){
+            z = z*z +c;
+            if(abs(z)>2){//if z exeed 2, it will go to infinity with iterations
+                break;
+            }
+        }
+
+
+
+        return 0;
+
     }
     
 }
